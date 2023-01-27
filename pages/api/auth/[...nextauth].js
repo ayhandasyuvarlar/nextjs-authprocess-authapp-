8b40dbb85302807b7d0cdefb42d12ff67e7f9c5e
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2023-01-27 17:21:20 +0300

    env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,23 +3,18 @@ import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
-const GOOGLE_CLIENT_ID =
-  "267409462275-tn7f85pnoslo3js9kr2qctpj22jsi0ru.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
-const GITHUB_ID = "58784e8a3af070db055f";
-const GITHUB_SECRET = "b03c23d7cba9e3056b055e763db05854256e82b2";
 import connectMongo from "@/database/connetMongo";
 import Users from "@/model/Schema";
 export default NextAuth({
   providers: [
     // Google Provider
     GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     GitHubProvider({
-      clientId: GITHUB_ID,
-      clientSecret: GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     CredentialsProvider({
       name: "credentials",
